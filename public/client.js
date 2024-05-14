b23c92a662d1cd33f0d2f6f8175739d2e7eefe11
Friedhof5rb <unconfigured@null.spigotmc.org>
2024-05-14 12:15:46 +0200

    improved network and ui

--- public/client.js
+++ public/client.js
@@ -1,5 +1,6 @@
 const socket = io();
 const canvas = document.getElementById('myCanvas');
+const customCursor = document.getElementById('customCursor');
 const ctx = canvas.getContext('2d');
 
 
@@ -9,7 +10,33 @@ document.addEventListener('keyup', handleKeyUp);
 const nameForm = document.getElementById('nameForm');
 const nameInput = document.getElementById('nameInput');
 
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
+  }
+  
+resizeCanvas();
+
+window.addEventListener('resize', resizeCanvas);
+
+
+canvas.addEventListener("mouseover", () => {
+    canvas.style.cursor = "none"; // Change cursor on mouseover
+    customCursor.style.display = 'block';
+  });
+  
+  canvas.addEventListener("mouseout", () => {
+    canvas.style.cursor = "default"; // Reset cursor on mouseout
+    customCursor.style.display = 'none';
+  });
+
+
+
+window.addEventListener('mousemove', (e) => {
+    customCursor.style.left = `${e.clientX}px`;
+    customCursor.style.top = `${e.clientY}px`;
+  });
 
 socket.on('connect', () => {
   socket.emit("requestAllObjectData");
@@ -42,7 +69,8 @@ class MyObject {
      
       ctx.beginPath();
       ctx.arc(this.x, this.y, 4, 0, 2 * Math.PI); // 
-      ctx.fillStyle = 'grey'; // Set the fill color
+      ctx.fillStyle = 'rgb(255,165,0)'; // Set the fill color
+      ctx.shadowColor = 'rgba(255,165,0,0.8)';
       ctx.fill(); // Draw the filled circle
   
     }
@@ -102,81 +130,141 @@ let pingStartTime = Date.now();
 let visiblePastBullets = [];
 let visiblePastObjects = [];
 //pixels per 100th second
-const speedOfLight  = 2;
+const speedOfLight  = 3;
 
 let isWPressed = false;
 let isAPressed = false;
 let isSPressed = false;
 let isDPressed = false;
 
-let isSpacePressed = false;
-let canPressSpace = true;
 
-
+const buffer = [];
+const MAX_BUFFER_SIZE = 200;
 
 let clientTime = 0;
 
 
+let testdatacount = 0;
 
 
+socket.on("buffered-data",(packet)=>{
 
-
-socket.on("synctime",(time)=>{
-
-clientTime = time;
-
-});
-
-socket.on("syncpast",(data)=>{
-    if (data instanceof ArrayBuffer) {
-        let binaryData = data;
-        let jsonString = new TextDecoder().decode(binaryData);
-        let array = JSON.parse(jsonString);
-        Players.forEach((player)=>{
-            let player2 = array.find(obj=>obj.id===player.id)
-            player.past = player.past.concat(player2.past); 
-            if(player.past.length > 1000){
-                let howMany = player.past.length-1000;
-                player.past.splice(0,howMany);
-            }
-        })
-
-    }
-   
-});
-
-socket.on("syncbullets",(data)=>{
-    if (data instanceof ArrayBuffer) {
-        let binaryData = data;
-        let jsonString = new TextDecoder().decode(binaryData);
-        let array = JSON.parse(jsonString);
-
-        array.forEach((bulletobj)=>{
-            let bulletIndex = Bullets.findIndex(obj=>obj.id===bulletobj.id)
-            if(Bullets[bulletIndex]){
-                Bullets[bulletIndex].past = Bullets[bulletIndex].past.concat(bulletobj.past); 
-                if(Bullets[bulletIndex].past.length > 500){
-                    let howMany = Bullets[bulletIndex].past.length-500;
-                    Bullets[bulletIndex].past.splice(0,howMany);
+    for (const dataObj of packet) {
+       let data = dataObj.data;
+       switch(dataObj.name){
+            case "synctime":
+                clientTime = data;
+            break;
+            case "syncpast":
+                if (data instanceof ArrayBuffer) {
+                    let binaryData = data;
+                    let jsonString = new TextDecoder().decode(binaryData);
+                    let array = JSON.parse(jsonString);
+                    Players.forEach((player)=>{
+                        let player2 = array.find(obj=>obj.id===player.id)
+                        player.past = player.past.concat(player2.past); 
+                        if(player.past.length > 1000){
+                            let howMany = player.past.length-1000;
+                            player.past.splice(0,howMany);
+                        }
+                    })
+            
                 }
-            }else{
-                Bullets.push(bulletobj);
-            }
-          
-        })
+            break;
+            case "syncbullets":
+                if (data instanceof ArrayBuffer) {
+                    let binaryData = data;
+                    let jsonString = new TextDecoder().decode(binaryData);
+                    let array = JSON.parse(jsonString);
+            
+                    array.forEach((bulletobj)=>{
+                        let bulletIndex = Bullets.findIndex(obj=>obj.id===bulletobj.id)
+                        if(Bullets[bulletIndex]){
+                            Bullets[bulletIndex].past = Bullets[bulletIndex].past.concat(bulletobj.past); 
+                            if(Bullets[bulletIndex].past.length > 500){
+                                let howMany = Bullets[bulletIndex].past.length-500;
+                                Bullets[bulletIndex].past.splice(0,howMany);
+                            }
+                        }else{
+                            Bullets.push(bulletobj);
+                        }
+                      
+                    })
+            
+                    for (let i = Bullets.length - 1; i >= 0; i--) {
+                        let checkBullet = array.find(obj=>obj.id===Bullets[i].id)
+                        if(!checkBullet){
+                            Bullets.splice(i,1);
+                        }
+                    }
+                }
+            break;
+            case "synchronizeEveryObject":
+                if (data instanceof ArrayBuffer) {
+                    let binaryData = data;
+                    let jsonString = new TextDecoder().decode(binaryData);
+                    let obj2 = JSON.parse(jsonString);
+            
+                    Players = obj2.Players;
+                }
+            break;
+            case "start":
+                if (data instanceof ArrayBuffer) {
+                    let binaryData = data;
+                    let jsonString = new TextDecoder().decode(binaryData);
+                    let obj2 = JSON.parse(jsonString);
+                    
+                    if(obj2.name == "player"){
+                        let player = Players.find(obj => obj.id === obj2.id);
+                        if(!player){
+                            Players.push(obj2)
+                           
+                        }
+                    }
+                  }
+                  mainPlayer = Players.find(obj=>obj.id === socket.id)
+            break;
+            case "updatePlayer":
+                if (data instanceof ArrayBuffer) {
+                    let jsonString = new TextDecoder().decode(data);
+                    let obj2 = JSON.parse(jsonString);
+                    
+                    let playerIndex = Players.findIndex(obj => obj.id === obj2.id);
+            
+                    let past =  Players[playerIndex].past;
+                    Players[playerIndex] = obj2;
+                    Players[playerIndex].past = past;
+                  }
+            break;
+            case "removePlayer":
+                let playerIndex = Players.findIndex(obj => obj.id === data);
+                Players.splice(playerIndex,1)
+            break;
+            default:
+                  console.log(dataObj.name + " not known.")
+       }
+      }
 
-        for (let i = Bullets.length - 1; i >= 0; i--) {
-            let checkBullet = array.find(obj=>obj.id===Bullets[i].id)
-            if(!checkBullet){
-                Bullets.splice(i,1);
-            }
-        }
-    }
-});
+   
+
+
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
 socket.on("synchronizeEveryObject",(objectDataBinary)=>{
+
+
     if (objectDataBinary instanceof ArrayBuffer) {
         let binaryData = objectDataBinary;
         let jsonString = new TextDecoder().decode(binaryData);
@@ -192,49 +280,9 @@ socket.on("synchronizeEveryObject",(objectDataBinary)=>{
 
 
 
-socket.on("start",(message)=>{
-
-    if (message instanceof ArrayBuffer) {
-        let binaryData = message;
-        let jsonString = new TextDecoder().decode(binaryData);
-        let obj2 = JSON.parse(jsonString);
-        
-        if(obj2.name == "player"){
-            let player = Players.find(obj => obj.id === obj2.id);
-            if(!player){
-                Players.push(obj2)
-               
-            }
-        }
 
 
 
-      }
-      mainPlayer = Players.find(obj=>obj.id === socket.id)
-});
-
-socket.on("updatePlayer",(binaryData)=> {
-
-    if (binaryData instanceof ArrayBuffer) {
-        let jsonString = new TextDecoder().decode(binaryData);
-        let obj2 = JSON.parse(jsonString);
-        
-        let playerIndex = Players.findIndex(obj => obj.id === obj2.id);
-
-        let past =  Players[playerIndex].past;
-        Players[playerIndex] = obj2;
-        Players[playerIndex].past = past;
-      }
-
-
-});
-
-socket.on("removePlayer", (id)=>{
-
-    let playerIndex = Players.findIndex(obj => obj.id === id);
-    Players.splice(playerIndex,1)
-
-});
 
 setInterval(sendPlayerMovement,50);
 
@@ -275,8 +323,14 @@ socket.on('pong',()=>{
 
 })
 
+/*
+setInterval(()=>{
+    console.log(testdatacount);
+    testdatacount = 0;
 
 
+},1000);
+*/
 
 
 
@@ -362,9 +416,6 @@ setInterval(()=>{
      visiblePastBullets= findVisiblePastBullets(Bullets, mainPlayer);
     }
    
-    if(isSpacePressed){
-        console.log(Bullets);
-    }
 
 
  },50);
@@ -373,11 +424,17 @@ setInterval(()=>{
 
 
 function animate() {
-   ctx.fillStyle  = "black"
+   ctx.fillStyle  = "#222"
    ctx.clearRect(0, 0, canvas.width, canvas.height);
    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
   
+   ctx.shadowBlur = 40;
+
+
    ctx.fillStyle  = "white"
+   ctx.shadowColor = 'rgba(255,255,255,0.8)';
    ctx.font = 'bold 12px Arial';
   
    ctx.fillText("ping: " + ping, 20, 60);
@@ -389,26 +446,29 @@ function animate() {
         ctx.fillText("x: " + player.x, 20, 20);
         ctx.fillText("y: " + player.y, 20, 40);
 
-
-
+        let centerX = -canvas.width/2+player.x;
+        let centerY = -canvas.height/2+player.y
 
         ctx.beginPath();
-        ctx.arc(player.x, player.y, 10, 0, 2 * Math.PI); // Radius of 20 pixels
-        ctx.fillStyle = 'red'; // Set the fill color
+        ctx.arc(player.x-centerX, player.y-centerY, 10, 0, 2 * Math.PI); 
+        ctx.fillStyle = 'white'; // Set the fill color
+        ctx.shadowColor = 'rgba(255,255,255,0.8)';
         ctx.fill(); // Draw the filled circle
         ctx.font = '16px Arial';
-        ctx.fillStyle = 'green';
-        ctx.fillText(player.playerName, player.x, player.y - 20);
-    
+        ctx.fillStyle = '#696969';
+        ctx.shadowColor = 'rgba(69,69,69,0.8)';
+        ctx.fillText(player.playerName, player.x-centerX, player.y - 20-centerY);
 
         visiblePastObjects.forEach(pos => {
             ctx.beginPath();
-            ctx.arc(pos.x, pos.y, 10, 0, 2 * Math.PI);
-            ctx.fillStyle = 'red'; // Set the fill color
+            ctx.arc(pos.x-centerX, pos.y-centerY, 10, 0, 2 * Math.PI);
+            ctx.fillStyle = 'white'; // Set the fill color
+            ctx.shadowColor = 'rgba(255,255,255,0.8)';
             ctx.fill(); // Draw the filled circle
             ctx.font = '16px Arial';
-            ctx.fillStyle = 'green';
-            ctx.fillText(pos.playerName, pos.x, pos.y - 20);
+            ctx.fillStyle = '#696969';
+            ctx.shadowColor = 'rgba(69,69,69,0.8)';
+            ctx.fillText(pos.playerName, pos.x-centerX, pos.y - 20-centerY);
 
 
         });
@@ -421,7 +481,7 @@ function animate() {
         visiblePastBullets.forEach(pos => {
 
           const obj = objectPool.acquire();
-          obj.update(pos.x,pos.y);
+          obj.update(pos.x-centerX,pos.y-centerY);
           obj.render();
           objectPool.release(obj);
 
@@ -432,9 +492,6 @@ function animate() {
 
     }
 
-            
-
-    
    
     requestAnimationFrame(animate);
   }
@@ -454,10 +511,7 @@ function animate() {
         isDPressed = true;
     }
 
-    if(event.key === " "){
-        isSpacePressed = !isSpacePressed;
-        canPressSpace = false;
-    }
+   
 
 
 
@@ -480,9 +534,7 @@ function animate() {
         isDPressed = false;
     }
 
-    if(event.key === " "){
-        canPressSpace = true;
-    }
+    
   }
 
   nameForm.addEventListener('submit', (event) => {
@@ -503,15 +555,15 @@ function animate() {
 
         let player = Players.find(obj=>obj.id === socket.id)
         if(player){
-            let centerX = player.x;
-            let centerY = player.y;
+            let centerX = canvas.width/2;
+            let centerY = canvas.height/2;
 
             const mouseX = event.clientX;
             const mouseY = event.clientY;
         
             const dx = mouseX - centerX;
             const dy = mouseY - centerY;
-        
+
             const angle = Math.atan2(dy, dx);
 
             let bulletdata = {id:socket.id,angle:angle}
--- src/server.js
+++ src/server.js
@@ -14,6 +14,10 @@ let Bullets = [];
 
 let nextBulletId = 0;
 
+let testdatacount = 0;
+
+const buffer = [];
+const MAX_BUFFER_SIZE = 4;
 
 //pixels per 10th second, playerSpeed/10 = vergelichbar mit lightspeed
 let playerSpeed = 10;
@@ -24,8 +28,6 @@ let time = 0;
 io.on("connect",(socket) => {
 
     console.log("User connected:",socket.id);
-
-
 
 
     socket.on("spawnPlayer",(playerName)=>{
@@ -42,7 +44,8 @@ io.on("connect",(socket) => {
         if(Players[playerIndex]){
             Players[playerIndex].y -= playerSpeed/2;
             let playerBinary = convertDataToBinary( Players[playerIndex]);
-            io.emit("updatePlayer",playerBinary);
+            //io.emit("updatePlayer",playerBinary);
+            buffer.push({name:"updatePlayer",data:playerBinary})
         }
     })
     socket.on("aPressed",() => {
@@ -50,33 +53,40 @@ io.on("connect",(socket) => {
         if(Players[playerIndex]){
             Players[playerIndex].x -= playerSpeed/2;
             let playerBinary = convertDataToBinary( Players[playerIndex]);
-            io.emit("updatePlayer",playerBinary);
+            //io.emit("updatePlayer",playerBinary);
+            buffer.push({name:"updatePlayer",data:playerBinary})
         }
     })
     socket.on("sPressed",() => {
+      
         let playerIndex = Players.findIndex(obj => obj.id === socket.id);
         if(Players[playerIndex]){
             Players[playerIndex].y += playerSpeed/2;
             let playerBinary = convertDataToBinary( Players[playerIndex]);
-            io.emit("updatePlayer",playerBinary);
+            //io.emit("updatePlayer",playerBinary);
+            buffer.push({name:"updatePlayer",data:playerBinary})
         }
     })
     socket.on("dPressed",() => {
+        
         let playerIndex = Players.findIndex(obj => obj.id === socket.id);
         if(Players[playerIndex]){
             Players[playerIndex].x += playerSpeed/2;
             let playerBinary = convertDataToBinary( Players[playerIndex]);
-            io.emit("updatePlayer",playerBinary);
+            //io.emit("updatePlayer",playerBinary);
+            buffer.push({name:"updatePlayer",data:playerBinary})
         }
     })
 
     socket.on("requestAllObjectData",()=>{
+       
         let data = {Players:Players};
         let objectDataBinary = convertDataToBinary(data);
         socket.emit("synchronizeEveryObject",objectDataBinary);
     });
 
     socket.on("bullet",(bulletdata)=>{
+       
             let player = Players.find(obj => obj.id ===bulletdata.id)
             SpawnBullet(player,bulletdata.angle);
     })
@@ -91,7 +101,8 @@ io.on("connect",(socket) => {
         let playerIndex = Players.findIndex(obj => obj.id === socket.id);
         if(Players[playerIndex]){
             Players.splice(playerIndex,1)
-            io.emit("removePlayer",socket.id)
+            //io.emit("removePlayer",socket.id)
+            buffer.push({name:"removePlayer",data:socket.id})
         }
     });
 
@@ -102,21 +113,19 @@ function spawnPlayer(socket,playerName){
     playerObject = {name:"player",id:socket.id,x:300,y:300,playerName:playerName,past:[]}
     Players.push(playerObject);
 
-  
-
-
     //sending
     let player = Players.find(obj => obj.id === socket.id);
     let playerBinary = convertDataToBinary(player);
-    io.emit("start", playerBinary);
+    //io.emit("start", playerBinary);
+    buffer.push({name:"start",data:playerBinary})
 
 }
 
 setInterval(()=>{
 
     time += 1;
-    io.emit("synctime",time);
-
+    //io.emit("synctime",time);
+    buffer.push({name:"synctime",data:time})
     Players.forEach(player => {
 
         let positionST = {x:player.x,y:player.y,time:time}
@@ -131,7 +140,8 @@ setInterval(()=>{
     
       let data = convertDataToBinary(Players);
 
-      io.emit("syncpast",data)
+      //io.emit("syncpast",data)
+      buffer.push({name:"syncpast",data:data})
       Players.forEach(player => {
         player.past = [];
       });
@@ -171,8 +181,8 @@ setInterval(()=>{
     
       let data = convertDataToBinary(Bullets);
 
-      io.emit("syncbullets",data);
-
+      //io.emit("syncbullets",data);
+      buffer.push({name:"syncbullets",data:data})
       Bullets.forEach(bullet => {
         bullet.past = [];
       });
@@ -183,9 +193,8 @@ setInterval(()=>{
 
 
 
-
 function SpawnBullet(player,angle){
-    let magnitude = 3;
+    let magnitude = 5;
     const vx = magnitude * Math.cos(angle);
     const vy = magnitude * Math.sin(angle);
 
@@ -196,10 +205,30 @@ function SpawnBullet(player,angle){
     //console.log(nextBulletId);
 }
 
+function sendBufferedData() {
+    // Combine the buffered data into a single packet
+    const packet = buffer.slice(); // Create a copy to avoid modifying the original buffer
+    
+    // Send the packet over Socket.IO
+    io.emit('buffered-data', packet);
+    
+    // Clear the buffer after sending
+    buffer.length = 0;
+  }
 
+  setInterval(()=>{
 
+    if (buffer.length >= MAX_BUFFER_SIZE) {
+        // Buffer is full, send the data
+        sendBufferedData();
+      }
 
+  },10);
 
+  setInterval(()=>{
+    sendBufferedData();
+  },1000);
+  
 
 //Host Server
 app.use(express.static("../public"));
